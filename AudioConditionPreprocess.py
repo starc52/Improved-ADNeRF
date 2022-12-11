135ea0ba1c807ea1cf8295ef85c55cee152f5518
starc52 <cstarpowful@gmail.com>
2022-12-10 23:14:19 -0500

    added multi-processing to audcond_preprocess

--- AudioConditionPreprocess.py
+++ AudioConditionPreprocess.py
@@ -1,12 +1,12 @@
 import os
 from tqdm import tqdm
 import pandas as pd
-
+import argparse
 
 def extract_audio(video_path):
     video_name = video_path.split('/')[-1]
     id_path = '/'.join(video_path.split('/')[:-1])
-    wav_file = os.path.join(id_path, video_name[:-3]+'wav')
+    wav_file = os.path.join(id_path, video_name[:-3] + 'wav')
     extract_wav_cmd = 'ffmpeg -i ' + video_path + ' -f wav -ar 16000 ' + wav_file
     os.system(extract_wav_cmd)
 
@@ -16,23 +16,29 @@ def extract_audio_features(id_path):
     os.system(extract_ds_cmd)
 
 
-def video_to_audio_features(csv_path):
+def video_to_audio_features(csv_path, frac=(1/6), frac_id=0):
     df = pd.read_csv(csv_path, index_col=0)
     idxs = df['idx'].unique()
-    uniq_vid_paths = []
-    for idx in tqdm(idxs):
+    list_idxs = [idxs[start:start+int(len(idxs)*frac)] for start in range(0, len(idxs), int(len(idxs)*frac))]
+    new_idxs = list_idxs[frac_id]
+    for idx in tqdm(new_idxs):
         idx_df = df[df['idx'] == idx]
         idx_path = '/'.join(idx_df.iloc[0]['path'].split('/')[:-2])
         for index, row in idx_df.iterrows():
-            video_path = '/'.join(row['path'].split('/')[:-1])+'.mp4'
-            audio_path = video_path[:-3]+'wav'
-            if video_path not in uniq_vid_paths and not os.path.isfile(audio_path):
+            video_path = '/'.join(row['path'].split('/')[:-1]) + '.mp4'
+            audio_path = video_path[:-3] + 'wav'
+            if not os.path.isfile(audio_path):
                 extract_audio(video_path)
-                uniq_vid_paths.append(video_path)
-            
-        extract_audio_features(idx_path)
+        videos_in_id = [ele for ele in os.listdir(idx_path) if os.path.isfile(ele) and ele[-3:] == "mp4"]
+        if not os.path.isfile(os.join(idx_path, videos_in_id[0][:-3] + 'npy')):
+            extract_audio_features(idx_path)
 
 
 if __name__ == '__main__':
-    video_to_audio_features('/scratch/tan/train_landmarks.csv')
-    video_to_audio_features('/scratch/tan/val_landmarks.csv')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--frac', type=float,
+                        default=(1/6), help='fraction to break he df into, for multi-processing')
+    parser.add_argument('--frac_id', type=int,
+                        default=0, help='frac_id to know which fraction to run')
+    video_to_audio_features('/scratch/tan/train_landmarks.csv', frac=parser.frac, frac_id=parser.frac_id)
+    video_to_audio_features('/scratch/tan/val_landmarks.csv',  frac=parser.frac, frac_id=parser.frac_id)
