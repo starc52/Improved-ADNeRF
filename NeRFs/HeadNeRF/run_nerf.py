4be3393c516f7eab7d58bae1f61ea66bb50c5bdf
starc52 <cstarpowful@gmail.com>
2022-12-15 04:50:56 +0530

    stray changes from server

--- NeRFs/HeadNeRF/run_nerf.py
+++ NeRFs/HeadNeRF/run_nerf.py
@@ -988,7 +988,9 @@ def train():
             os.makedirs(testsavedir, exist_ok=True)
             print('test poses shape', poses[i_val].shape)
             auds_val = AudNet(auds[i_val])
-            rof_embs = torch.cat([rof_emb] * auds.size(0), 0)
+
+            rof_embs = torch.cat([rof_emb] * auds_val.size(0), 0)
+
             auds_val = torch.cat([auds_val, rof_embs], 1)
             with torch.no_grad():
                 render_path(torch.Tensor(poses[i_val]).to(
